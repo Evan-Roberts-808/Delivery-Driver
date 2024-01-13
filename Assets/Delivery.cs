030b9274487a4247eaaff8a54c5f73ee08c8c83e
Evan Roberts <eroberts1998@icloud.com>
2024-01-13 07:40:17 -0500

    add functionality to pick up and deliver

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -1,15 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] float destroyDelay = 0.5f;
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+
+    bool hasPackage;
+    SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("ya crushed!");
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("I can't believe you drove over me");
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Package" && !hasPackage)
+        {
+            Debug.Log("Package Picked Up");
+            hasPackage = true;
+            Debug.Log(hasPackage);
+            Destroy(other.gameObject, destroyDelay);
+            spriteRenderer.color = hasPackageColor;
+        }
+        else if (other.tag == "Customer" && hasPackage)
+        {
+            Debug.Log("Package Delivered");
+            hasPackage = false;
+            Debug.Log(hasPackage);
+            spriteRenderer.color = noPackageColor;
+        }
     }
 }
