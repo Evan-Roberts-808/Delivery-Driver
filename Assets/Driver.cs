5d97c7c55a1c6dbed717b4e0681e1886a0b55311
Evan Roberts <eroberts1998@icloud.com>
2024-01-13 08:03:24 -0500

    add speed boosts and slow down on boost and collision

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -7,12 +7,9 @@ public class Driver : MonoBehaviour
     // Variables
     [SerializeField] float steerSpeed = 300f;
     [SerializeField] float moveSpeed = 20f;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 30f;
 
-    }
 
     // Update is called once per frame
     void Update()
@@ -21,5 +18,33 @@ public class Driver : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+
+    IEnumerator ResetSpeedAfterDelay(float originalSpeed)
+    {
+        yield return new WaitForSeconds(2f);
+        Debug.Log("back to original Speed");
+        moveSpeed = originalSpeed;
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        float originalSpeed = moveSpeed;
+        Debug.Log("Slow down");
+        moveSpeed = slowSpeed;
+        
+        StartCoroutine(ResetSpeedAfterDelay(originalSpeed));
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Boost")
+        {
+            Debug.Log("Speed Boost");
+            float originalSpeed = moveSpeed;
+            moveSpeed = boostSpeed;
+
+            StartCoroutine(ResetSpeedAfterDelay(originalSpeed));
+        }
+
     }
 }
